c8165178bed55e1fe103bdc7b832e6d1e8ff8ccc
programador <programador@VH203-16.upc.edu.pe>
2019-09-25 22:41:38 -0500

    second commit

---
+++ autoplanet/src/main/java/com/platform/entity/Cliente.java
@@ -0,0 +1,117 @@
+package com.upc.model.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "nombre", length = 120, nullable = false)
+	private String nombre;
+	
+	@Column(name = "direccion", length = 200, nullable = false)
+	private String direccion;
+	
+	@Column(name = "pais", nullable = false)
+	private String pais;
+	
+	@Column(name = "dni", length = 8, nullable = false)
+	private String dni;
+	
+	@Column(name = "telefono", length = 9, nullable = false)
+	private String telefono;
+	
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "cliente")
+	private User user;
+
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Cita> citas;
+
+	public Cliente() {
+		citas = new ArrayList<>();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<Cita> getCitas() {
+		return citas;
+	}
+
+	public void setCitas(List<Cita> citas) {
+		this.citas = citas;
+	}
+
+}
