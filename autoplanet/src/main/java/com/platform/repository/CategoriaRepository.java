19c0a710600d002be6138b33f4290a3c65868ca9
AMD <AMD@AMD-PC>
2019-09-27 18:11:50 -0500

    categoria terminado

---
+++ autoplanet/src/main/java/com/platform/repository/CategoriaRepository.java
@@ -0,0 +1,40 @@
+package com.platform.repository;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import com.platform.entity.Categoria;
+
+@Named
+public class CategoriaRepository implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@PersistenceContext(unitName = "visorPU")
+	private EntityManager em;
+
+	public Integer insert(Categoria categoria) throws Exception {
+		em.persist(categoria);
+		return categoria.getId();
+	}
+
+	public Integer update(Categoria categoria) throws Exception {
+		em.merge(categoria);
+		return categoria.getId();
+	}
+
+	public List<Categoria> findAll() throws Exception {
+		List<Categoria> categorias = new ArrayList<>();
+
+		TypedQuery<Categoria> query = em.createQuery("SELECT c FROM Categoria c", Categoria.class);
+		categorias = query.getResultList();
+		return categorias;
+	}
+
+}
--- autoplanet/src/main/java/com/platform/repository/ClienteRepository.java
+++ autoplanet/src/main/java/com/platform/repository/ClienteRepository.java
@@ -1,5 +1,50 @@
 package com.platform.repository;
 
-public class ClienteRepository {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import com.platform.entity.Cliente;
+
+@Named
+public class ClienteRepository implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	@PersistenceContext(unitName = "visorPU")
+	private EntityManager em;
+
+	public Integer insert(Cliente cliente) throws Exception {
+		em.persist(cliente);
+		return cliente.getId();
+	}
+	
+	public Integer update(Cliente cliente) throws Exception {
+		em.merge(cliente);
+		return cliente.getId();
+	}
+
+	public Optional<Cliente> findByDni(String dni) throws Exception {
+		Cliente cliente;
+		TypedQuery<Cliente> customerFound = em.createQuery("Select c from Cliente c WHERE c.dni =?1",
+				Cliente.class);
+		customerFound.setParameter(1, dni);
+		cliente = customerFound.getSingleResult();
+
+		return Optional.of(cliente);
+	}
+	
+	public List<Cliente> findAll() throws Exception {
+		List<Cliente> clientes = new ArrayList<>();
+
+		TypedQuery<Cliente> query = em.createQuery("SELECT c FROM Cliente c", Cliente.class);
+		clientes = query.getResultList();
+		return clientes;
+	}
 
 }
